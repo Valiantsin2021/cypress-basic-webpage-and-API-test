cbff9869bbc5f4065249170006c5e3a0bfd82ede
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2022-08-21 20:42:31 +0200

    add simpleBookApi.cy.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,6 +4,26 @@ Cypress.on('uncaught:exception', (err) => {
     if (resizeObserverLoopErrRe.test(err.message)) {
         return false
     }
+    if (test.state === 'failed') {
+        let item = runnable
+        const nameParts = [runnable.title]
+    
+        // Iterate through all parents and grab the titles
+        while (item.parent) {
+          nameParts.unshift(item.parent.title)
+          item = item.parent
+        }
+    
+        const fullTestName = nameParts
+                .filter(Boolean)
+                .join(' -- ')           // this is how cypress joins the test title fragments
+    
+        const imageUrl = `screenshots/${
+          Cypress.spec.name
+        }/${fullTestName} (failed).png`
+    
+        addContext({ test }, imageUrl)
+      }
 })
 import '@testing-library/cypress/add-commands'
 require('cypress-iframe')
